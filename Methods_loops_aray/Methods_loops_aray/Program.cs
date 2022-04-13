0111af22bcb5437318b5eeb3589a30694a21d74e
Shahrom <shahromkr@code.edu.az>
2022-04-14 03:44:14 +0400

    added task about loops,methods,array

---
+++ Methods_loops_aray/Methods_loops_aray/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Methods_loops_aray
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+
+        {
+
+            string[] array = { "Stol", "Stul", "Pult", "telefon" };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                if (array[i].Length > 5)
+                {
+                    Console.WriteLine(array[i].Length);
+
+
+
+                    {
+                        Console.WriteLine("Ededi daxil edin");
+
+                    
+
+                int number = Convert.ToInt32(Console.ReadLine());
+                int sum = default(int);
+                int lastdigit;
+
+                while (number > 0)
+                {
+                    lastdigit = number % 10;
+                    number = number / 10;
+                    sum = sum + lastdigit;
+
+                }
+                Console.WriteLine(sum);
+
+            }
+        }
+
+    
+
+    
+
+    }
+}    
+
+
+            
+
+
+           
